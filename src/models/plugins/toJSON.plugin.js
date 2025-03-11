22b05975c856f87246e893a4927e73fe963ddf7b
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-03-11 08:55:48 +0530

    cnh

--- src/models/plugins/toJSON.plugin.js
+++ src/models/plugins/toJSON.plugin.js
@@ -13,6 +13,7 @@ const deleteAtPath = (obj, path, index) => {
   }
   deleteAtPath(obj[path[index]], path, index + 1);
 };
+
 const toJSON = (schema) => {
   let transform;
   if (schema.options.toJSON && schema.options.toJSON.transform) {
@@ -21,15 +22,17 @@ const toJSON = (schema) => {
 
   schema.options.toJSON = Object.assign(schema.options.toJSON || {}, {
     transform(doc, ret, options) {
-      if (!ret._id) {
-        console.error("toJSON Error: _id is undefined!", ret);
-      } else {
-        ret.id = ret._id.toString();
-      }
+      Object.keys(schema.paths).forEach((path) => {
+        if (schema.paths[path].options && schema.paths[path].options.private) {
+          deleteAtPath(ret, path.split('.'), 0);
+        }
+      });
+
+      ret.id = ret._id.toString();
       delete ret._id;
       delete ret.__v;
+      delete ret.createdAt;
       delete ret.updatedAt;
-
       if (transform) {
         return transform(doc, ret, options);
       }
