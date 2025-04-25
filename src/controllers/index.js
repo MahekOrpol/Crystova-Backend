670485df7df01157d1e6536c3fbc42e4ffed4dba
MahekOrpol <mahekorpolinfotech24@gmail.com>
2025-04-25 15:57:17 +0530

    delete

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,16 +1,8 @@
 module.exports.authController = require('./auth.controller');
 module.exports.adminController = require('./admin.controller');
 module.exports.userController = require('./user.controller');
-module.exports.gameController = require('./game.controller');
 module.exports.contactUsController = require('./contactUs.controller');
-module.exports.assignmentsController=require('./assignments.controller');
-module.exports.todosController = require('./todos.controller');
 module.exports.notificationController=require('./notifications.controller');
-module.exports.roomController=require('./room.controller');
-module.exports.chatController=require('./chat.controller');
-module.exports.taskController=require('./task.controller');
-module.exports.discussionRoomController=require('./discussionRoom.controller');
-module.exports.discussionChatController=require('./discussionChat.controller');
 module.exports.registerController=require('./register.controller')
 module.exports.productController=require('./product.controller')
 module.exports.wishlistController=require('./wishlist.controller')
