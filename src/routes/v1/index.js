caf63387ed7deaea9ba79da910824baff0c1f67c
mayanimahek007 <mayanimahek007@gmail.com>
2025-05-05 21:16:23 +0530

    delete other file

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,141 +1,40 @@
-const express = require("express");
-const userRoute = require("./user.route");
-const adminRoute = require("./admin.route");
-const authRoute = require("./auth.route");
-const gameRoute = require("./game.route");
-const aboutUsRoute = require("./aboutUs.route");
-const assignmentsRoute = require("./assignments.route");
-const toDosRoute = require("./todos.route");
-const eventRoute = require("./event.route");
-const resourcesRouter = require("./resources.route");
-const notificationRoute = require("./notification.route");
-const roomRoute = require("./room.route");
-const chatRoute = require("./chat.route");
-const taskRoute = require("./task.route");
-const discussionRoomRoute = require("./discussionRoom.route");
-const discussionChatRoute = require("./discussionChat.route");
-const registerRoute = require("./register.route");
-const productRoute=require('./products.route')
-const wishlistRoute=require('./wishlist.route')
-const categoryRoute=require('./category.route')
-const orderDetailsRoute=require('./orderDetails.route')
-const orderRoute=require('./order.route')
-const paymentRoute=require('./payment.route')
-const contactRoutes = require('./contactUs.route')
+const express = require('express');
 const router = express.Router();
-const priceFilterRoute=require('./priceFilter.route');
-const customJewelsRoute = require("./customJewels.route");
-const reviewsRoute = require("./review.route");
 
-const defaultRoutes = [
-  {
-    path: "/auth",
-    route: authRoute, 
-  },
-  {
-    path: "/event",
-    route: eventRoute,
-  },
-  {
-    path: "/admin",
-    route: adminRoute,
-  },
-  {
-    path: "/users",
-    route: userRoute,
-  },
-  {
-    path: "/todos",
-    route: toDosRoute,
-  },
-  {
-    path: "/resources",
-    route: resourcesRouter,
-  },
-  {
-    path: "/games",
-    route: gameRoute,
-  },
-  {
-    path: "/aboutUs",
-    route: aboutUsRoute,
-  },
-  {
-    path: "/assignments",
-    route: assignmentsRoute,
-  },
-  {
-    path: "/notification",
-    route: notificationRoute,
-  },
-  {
-    path: "/room",
-    route: roomRoute,
-  },
-  {
-    path: "/chat",
-    route: chatRoute,
-  },
-  {
-    path: "/task",
-    route: taskRoute,
-  },
-  {
-    path: "/discussionRoom",
-    route: discussionRoomRoute,
-  },
-  {
-    path: "/discussionChat",
-    route: discussionChatRoute,
-  },
-  {
-    path: "/register",
-    route: registerRoute,
-  },
-  {
-    path: '/product',
-    route: productRoute,
-  },
-  {
-    path: '/category',
-    route: categoryRoute,
-  },
-  {
-    path: '/wishlist',
-    route: wishlistRoute,
-  },
-  {
-    path: '/contact-us',
-    route: contactRoutes,
-  },
-  {
-    path: '/order-details',
-    route: orderDetailsRoute,
-  },
-  {
-    path: '/order',
-    route: orderRoute,
-  },
-  {
-    path: '/payment',
-    route: paymentRoute,
-  },
-  {
-    path:'/price-filter',
-    route: priceFilterRoute,
-  },
-  {
-    path:'/custom-jewels',
-    route: customJewelsRoute,
-  },
-  {
-    path:'/review',
-    route: reviewsRoute,
-  }
-];
+// Import all route files
+const authRoutes = require('./auth.route');
+const userRoutes = require('./user.route');
+const wishlistRoutes = require('./wishlist.route');
+const registerRoutes = require('./register.route');
+const reviewRoutes = require('./review.route');
+const paymentRoutes = require('./payment.route');
+const priceFilterRoutes = require('./priceFilter.route');
+const productsRoutes = require('./products.route');
+const orderRoutes = require('./order.route');
+const orderDetailsRoutes = require('./orderDetails.route');
+const notificationRoutes = require('./notification.route');
+const contactUsRoutes = require('./contactUs.route');
+const customJewelsRoutes = require('./customJewels.route');
+const categoryRoutes = require('./category.route');
+const adminRoutes = require('./admin.route');
+const assignmentsRoutes = require('./assignments.route');
 
-defaultRoutes.forEach((route) => {
-  router.use(route.path, route.route);
-});
+// Use all routes
+router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
+router.use('/wishlist', wishlistRoutes);
+router.use('/register', registerRoutes);
+router.use('/review', reviewRoutes);
+router.use('/payment', paymentRoutes);
+router.use('/priceFilter', priceFilterRoutes);
+router.use('/products', productsRoutes);
+router.use('/order', orderRoutes);
+router.use('/orderDetails', orderDetailsRoutes);
+router.use('/notification', notificationRoutes);
+router.use('/contactUs', contactUsRoutes);
+router.use('/customJewels', customJewelsRoutes);
+router.use('/category', categoryRoutes);
+router.use('/admin', adminRoutes);
+router.use('/assignments', assignmentsRoutes);
 
-module.exports = router;
+module.exports = router; 
